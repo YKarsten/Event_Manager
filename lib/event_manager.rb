ba888b2cb65cd552380a75b525d2da5e78afdc99
YKarsten <yannik.karsten@web.de>
2022-12-01 16:21:54 +0100

    Add a method to clean up homephone numbers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,7 +3,20 @@ require 'google/apis/civicinfo_v2'
 require 'erb'
 
 def clean_zipcode(zipcode)
-zipcode.to_s.rjust(5, '0')[0..4]
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def clean_phone_number(phone_number)
+  phone_number = phone_number.tr('^0-9', '')
+  if phone_number.length < 10 || phone_number.length > 11
+    '0000000000'
+  elsif phone_number.length == 11 && phone_number[0] != '1'
+    '0000000000'
+  elsif phone_number.length == 11 && phone_number[0] == '1'
+    phone_number[1..10]
+  else
+    phone_number
+  end
 end
 
 def legislators_by_zipcode(zip)
@@ -11,7 +24,7 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
@@ -25,12 +38,11 @@ def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
-  
-  File.open(filename, "w") do |file|
+
+  File.open(filename, 'w') do |file|
     file.puts form_letter
   end
 end
-
 
 puts 'Event Manager initialized!'
 
@@ -44,15 +56,19 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 contents.each do |row|
-  id =row[0]
+  id = row[0]
   name = row[:first_name]
-  
+
   zipcode = clean_zipcode(row[:zipcode])
+
+  phone_number = clean_phone_number(row[:homephone])
+  # phone_number = row[:homephone]
+
+  puts phone_number
 
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, form_letter)
-
 end
